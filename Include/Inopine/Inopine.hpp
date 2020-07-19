11f3db77a4554fd392cda0f3923541aad4c8b36c
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2020-07-19 23:37:27 +0200

    Added Foundation Code

---
+++ Include/Inopine/Inopine.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/*
+
+    +-----------------------------+
+    | Inopine Engine Include File |
+    +-----------------------------+
+
+    Project........Inopine Engine
+    Author.........PolarToCartesian
+    Repository.....https://www.github.com/PolarToCartesian/Inopine
+    C++ Version....C++20
+
+    Thanks to:
+    - 
+    - 
+    - 
+
+*/
+
+/* The "IE" namespace contains all of Inopine Engine's source code in order       */
+/* to prevent the clashing of declarations and definitions in the root namespace. */
+namespace IE {
+
+}; // IE
