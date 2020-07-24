10bd112e01745905e3f2ce97655e8d3addc5b608
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2020-07-25 00:08:06 +0200

    Simple Window Test

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -1,13 +1,46 @@
 #include <Inopine/Inopine.hpp>
 #include <numbers>
+#include <chrono>
+#include <thread>
 
 int main(int argc, char** argv) {
     //std::cout << IE::Matf32::MakeRotationX(std::numbers::pi) << '\n';
     
-    ::IE::Window window(500, 500, "yo le rap");
+    ::IE::Window window(500, 500, "Inopine Test Window");
 
+    auto s = window.GetClientDimensions();
+    bool prev = false;
     while (window.IsRunning()) {
         window.Update();
+
+        if (s != window.GetClientDimensions())
+        {
+            s = window.GetClientDimensions();
+            std::cout << s << '\n';
+        }
+    
+        if (!prev && window.IsKeyDown('A'))
+        {
+            prev = true;
+            std::cout << "A IS DOWN\n";
+        }
+
+        if (prev && window.IsKeyUp('A')) {
+            prev = false;
+            std::cout << "A IS UP\n";
+        }
+
+        if (window.IsLeftButtonDown()) {
+            std::cout << "LEFT DOWN\n";
+        }
+
+        if (window.IsRightButtonDown()) {
+            std::cout << "RIGHT DOWN\n";
+        }
+
+        std::cout << window.GetRelativeCursorPosition() << '\n';
+
+        std::this_thread::sleep_for(std::chrono::microseconds(16));
     }
 
     return 0;
